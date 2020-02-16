cdd080c76eb98a0c7680e9f69dbd2e05e1b6c613
John Lownie <jlownie@gmail.com>
2020-02-16 10:36:06 -0500

    updates

--- src/app/services/pipeline.service.ts
+++ src/app/services/pipeline.service.ts
@@ -9,24 +9,7 @@ export class PipelineService {
   private styleSlider = new BehaviorSubject<boolean>(false);
   isStyleSliderSet = this.styleSlider.asObservable();
 
-<<<<<<< HEAD
   constructor() { }
-=======
-  constructor(private http: HttpClient) { }
-
-  public getPipelines(): Observable<any> {
-    console.log("Returning pipelines: " + environment.apiUrl);
-    return this.http.get(environment.apiUrl + "/pipelines");
-  }
-
-  public getPipeline(id: number): Observable<any> {
-    return this.http.get(environment.apiUrl + "/pipelines/" + id);
-  }
-
-  public getDefaultPipeline(): Observable<any> {
-    return this.getPipeline(1);
-  }
->>>>>>> c2051d7ee3cda8d4a85640a06da3a34b2a5b3f8b
 
   public toggleSliderStyle() {
     this.styleSlider.next(!this.styleSlider.value);
